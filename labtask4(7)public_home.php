2023242397bd84c0a9857e111060598b62fcd0ce
isratar <90677282+isratar@users.noreply.github.com>
2021-10-17 21:14:48 +0600

    Add files via upload

---
+++ labtask4(7)public_home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>K Company</title>
+</head>
+<body>
+	<?php 
+session_start();
+	echo "<div>";include 'basic/header.php';echo "</div>";
+ ?>
+	<!-- <?php include 'resources/header.php';?> -->
+	<br><br>
+<h1>Welcome to K Company</h1>
+<br><br><br><br><br><br>
+<div><?php include 'basic/footer.php';?></div>
+</body>
+</html>
